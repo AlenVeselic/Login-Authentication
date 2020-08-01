16618d2acac810be95960af83a30b74e07185e43
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2020-08-01 19:41:32 +0200

    Basic auth
    Basic authentication has been made functional:
    - the function iterates through a base of users comparing wether a user exists, if they exist, we go on to check if the password is correct
    
    - if the loop reaches the end of the userbase without a match it tells the users that that account doesn't exists and prompts them to create an account
    
    -text is added based on the user's input

--- js/Auth.js
+++ js/Auth.js
@@ -1,11 +1,43 @@
 user={name:"admin", password:"admin"}
 user2={name:"Jimi333",password:"12345"}
+user3={name:"Trevor", password:"sixtynine"}
 
 userBase=[];
 userBase.push(user);
 userBase.push(user2);
-function validateParams(){
-    
-    document.getElementById("bleh").innerHTML+=userBase[0].name;
+userBase.push(user3);
+
+function addUser(name,password){
+
+
+
+    newUser={name:String(name),password:String(password)}
 
 }
+
+function validateParams(){
+
+    username=document.getElementsByName("name")[0].value;
+    password=document.getElementsByName("pwd")[0].value;
+    match=false;
+
+  
+    for (x=0;x<userBase.length;x++){
+        if(username==userBase[x].name){
+            if(userBase[x].password==password){
+                
+                document.getElementById("loginForm").innerHTML+="Welcome!";
+                match=true;
+                
+            }else{
+                document.getElementById("loginForm").innerHTML+="Wrong password!!!";
+                match=true;
+                
+            }
+        }else if(x==userBase.length-1 && !match){
+            document.getElementById("loginForm").innerHTML+="<p> An account with this username doesn't exist. Would you like to create one?</p>";
+        }
+    }
+
+    document.getElementById("bleh").innerHTML="You entered: <br>" + "Username: "+username+"<br> Password: "+ password + "<br> Number of users: " + userBase.length;
+};
