7b123849a95c6acb96117fca0c456c19ad11146f
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2020-08-03 19:59:43 +0200

    Register and Storage
    -made Register.html functional
    -played a bit with session storage
    -realised I'm going to have to make a parser if I am to move my little userbase between sites using session storage

--- js/Auth.js
+++ js/Auth.js
@@ -7,18 +7,88 @@ userBase.push(user);
 userBase.push(user2);
 userBase.push(user3);
 
-function addUser(name,password){
 
+/*
+This code, along with the buttons on the pages, was used to test how session storage works
 
+function setValue(value){
 
-    newUser={name:String(name),password:String(password)}
+    sessionStorage.setItem("customValue",value);
 
 }
 
-function validateParams(){
+function getValue(){
+
+    alert(sessionStorage.getItem("customValue"));
+}
+
+*/ 
+
+function addUser(){
+    /*Fresh=false;
+    if(sessionStorage.getItem("users")!=null && !Fresh){
+        userBase=sessionStorage.getItem("users");
+    }else{
+        fresh=true;
+    }*/
+
+    alert(sessionStorage.users[0].name);
+
+
+
+    form=document.getElementById("registerForm");
+    warn=document.createElement("table");
+    warn.innerHTML="<tbody>";
+    for(gg=0;gg<userBase.length;gg++){
+
+        warn.innerHTML+="<tr><td>"+userBase[gg].name+"</td><td>"+userBase[gg].password+"</td></tr>";
+        
+    }
+
+    warn.innerHTML+="</tbody>";
+
+    form.parentNode.insertBefore(warn,form);
 
     username=document.getElementsByName("name")[0].value;
-    password=document.getElementsByName("pwd")[0].value;
+    password= document.getElementsByName("pwd")[0].value;
+
+    
+    if(!userExists(username)){
+    newUser={name:String(username),password:String(password)}
+    userBase.push(newUser);
+}else{
+    warn=document.createElement("p");
+    warn.innerHTML="THIS ACCOUNT ALREADY EXISTS. MAKE UP SOMETHING ORIGINAL!"
+    form.parentNode.insertBefore(warn,form.nextSibling);
+}
+
+    sessionStorage.setItem("users",userBase);
+}
+
+function verifyPassword(){
+    password= document.getElementsByName("pwd")[0].value;
+    verifyField=document.getElementsByName("vPwd")[0];
+    verifyFieldvalue=document.getElementsByName("vPwd")[0].value;
+
+    if(password!=verifyFieldvalue){
+    warn=document.createElement("p");
+    warn.innerHTML="wrong";
+    verifyField.parentNode.insertBefore(warn,verifyField.nextSibling);
+}
+    
+    
+}
+
+function validateParams(){
+    Fresh=false;
+    if(sessionStorage.getItem("users")!=null && !Fresh){
+    userBase=sessionStorage("users");
+}else{
+    Fresh=true;
+}
+    username=document.getElementsByName("name")[0].value;
+    password= document.getElementsByName("pwd")[0].value;
+
     match=false;
 
   
@@ -41,3 +111,15 @@ function validateParams(){
 
     document.getElementById("bleh").innerHTML="You entered: <br>" + "Username: "+username+"<br> Password: "+ password + "<br> Number of users: " + userBase.length;
 };
+
+function userExists(username){
+
+    match=false;
+    for (x=0;x<userBase.length;x++){
+        if(username==userBase[x].name){
+            match=true;
+        }
+    }
+
+    return match;
+}
